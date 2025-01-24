d98cc250b93f1b1208b780e9402fb3e67f6e4e61
wozien <571455773@qq.com>
2025-01-24 23:00:13 +0800

    chore: 删除无用代码

--- src/lib/const.tsx
+++ src/lib/const.tsx
@@ -1,16 +1,6 @@
 import { ListTodo, NotebookPen, Bookmark } from "lucide-react";
 import { TodoDifficulty } from "@prisma/client";
 
-export const ListMap = new Map([
-  ["rose", ["bg-rose-400", "玫瑰"]],
-  ["amber", ["bg-amber-400", "琥珀"]],
-  ["orange", ["bg-orange-400", "橘橙"]],
-  ["green", ["bg-lime-400", "草绿"]],
-  ["cyan", ["bg-sky-400", "天蓝"]],
-  ["indigo", ["bg-indigo-400", "葡紫"]],
-  ["pink", ["bg-pink-400", "粉粉"]],
-]);
-
 export const enum AppNameEnum {
   TODOLIST = "todolist",
   NOTE = "note",
